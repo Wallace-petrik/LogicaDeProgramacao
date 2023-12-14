e8dff288a076f0e70cd8123c1d7a60cad21aa622
Wallace Petrik <wallacepetrik@gmail.com>
2023-12-14 16:14:02 -0300

    Exercício 6
    Exercício 06 do capítulo 4

---
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosDeFixacao/ExerciciosPropostos06/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define tam 5
+
+
+
+int main(){
+    setlocale(LC_ALL,"");
+
+    int vetA[tam],vetB[tam],vetC[tam];
+
+    printf("\nEntre com o dados o primeiro vetor:");
+    for(int i = 0; i < tam; i++){
+        scanf("%d",vetA[i]);
+    }
+    printf("\nEntre com o dados o segundo vetor:");
+    for(int i = 0; i < tam; i++){
+        scanf("%d",vetB[i]);
+    }
+
+    bubbleSort(vetA,tam);
+    return 0;
+}
+
+void bubbleSort (int *vet, int tan){
+		int continua;
+		do{
+				continua = 0;
+				for(int i = 0; i < tan-1; i++){
+						if(vet[i] > vet[i+1]){
+								vet[i] = vet[i] + vet[i+1];
+								vet[i+1] = vet[i] - vet[i+1];
+								vet[i] = vet[i] - vet[i+1];
+								continua = i;
+						}
+				}
+				tan--;
+		}while(continua != 0);
+}
