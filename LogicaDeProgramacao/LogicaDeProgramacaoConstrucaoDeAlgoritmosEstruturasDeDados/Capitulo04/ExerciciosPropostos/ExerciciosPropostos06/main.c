37f8f3548f3a20485f14715e1eb351d2f6e1ae4b
Wallace Petrik <wallacepetrik@gmail.com>
2023-12-15 10:09:49 -0300

    Exercício 06
    Exercício proposto 06 do capítulo 4

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos06/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos06/main.c
@@ -8,26 +8,43 @@
 int main(){
     setlocale(LC_ALL,"");
 
-    int vetA[tam],vetB[tam],vetC[tam];
+    int vetA[tam],vetB[tam],vetC[tam],contador = 0;
 
     printf("\nEntre com o dados o primeiro vetor:");
     for(int i = 0; i < tam; i++){
         scanf("%d",&vetA[i]);
     }
-    /*printf("\nEntre com o dados o segundo vetor:");
+    printf("\nEntre com o dados o segundo vetor:");
     for(int i = 0; i < tam; i++){
-        scanf("%d",vetB[i]);
-    }*/
+        scanf("%d",&vetB[i]);
+    }
 
     bubbleSort(vetA,tam);
+    bubbleSort(vetB,tam);
+
+    for(int i = 0; i < tam; i++){
+        for(int j = 0; j < tam; j++){
+            if(vetA[i]==vetB[j]){
+                if(vetA[i]!=vetC[i] && vetC[contador-1]!=vetA[i]){
+                    vetC[contador]=vetA[i];
+                    contador++;
+                }
+            }
+        }
+    }
+    if(contador>0){
+        imprimeVet(vetC,contador);
+    }else{
+        printf("\nNão ha intersecção!");
+    }
     return 0;
 }
 
-void bubbleSort (int *vet, int tam){
+void bubbleSort (int *vet, int fim){
 		int continua;
 		do{
 				continua = 0;
-				for(int i = 0; i < tam-1; i++){
+				for(int i = 0; i < fim-1; i++){
 						if(vet[i] > vet[i+1]){
 								vet[i] = vet[i] + vet[i+1];
 								vet[i+1] = vet[i] - vet[i+1];
@@ -35,6 +52,15 @@ void bubbleSort (int *vet, int tam){
 								continua = i;
 						}
 				}
-				tam--;
+				fim--;
 		}while(continua != 0);
 }
+
+void imprimeVet(int *vet, int tamanho){
+    for(int i = 0; i < tamanho; i++){
+        printf("[%d] ",vet[i]);
+    }
+    printf("\n\n");
+}
+
+
