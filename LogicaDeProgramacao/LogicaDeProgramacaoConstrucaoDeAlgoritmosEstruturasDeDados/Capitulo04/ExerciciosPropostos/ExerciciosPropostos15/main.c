4c7e0faa2a2d38b89d76d1cfd444a697af1fef3c
Wallace Petrik <wallacepetrik@gmail.com>
2024-01-09 14:04:36 -0300

    Estruturas de Dados Heterogêneas - Registros
    Capítulo 4, Exercício proposto 15c

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos15/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos15/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 
 #define TAM_MAX 3
 
@@ -17,11 +18,14 @@ typedef struct{
 
 
 void bubblesort(Obras livros[TMP_MAX]);
-void buscarLivros(Obras livros[TAM_MAX], int codigoDeBusca);
+int buscarLivros(Obras livros[TAM_MAX], int codigoDeBusca);
+int buscarLivrosPorNome(Obras livros[TAM_MAX], char nomeDoLivrosDeBusca[40]);
+
 int main(){
     setlocale(LC_ALL,"");
 
     int codigo,opcao,area;
+    char nome[40];
 
     Obras cienciasExatas[TAM_MAX] = {75,155,"Banana Rei","Calor Machado","Book","Reis",8,50,"ABC","Machado","Book","Leandra",22,300,"C++","wallace","Alura","Berenice"};
     Obras cienciasHumanas[TAM_MAX] = {80,290,"Rei macaco","Maria Clara","Darkside","CLara",15,70,"Design Patterns","Wallace","Saraiva","Rita",46,255,"Java","IGor","Alura","Ana"};
@@ -31,66 +35,149 @@ int main(){
     bubblesort(cienciasExatas);
     bubblesort(cienciasHumanas);
 
-    do{
+        do{
         printf("\nESCOLHA UMA OPÇÂO\n");
-        printf("\n0/P voltar para o menu anterior");
-        printf("\n1/P buscar um livro: ");
+        printf("\n1/P buscar um livro código");
+        printf("\n2/P buscar um livro nome");
+        printf("\n10/P Sair: ");
             scanf("%d",&opcao);
 
         switch(opcao){
-            case 0:
-                printf("\nAté logo.");
-            break;
             case 1:
-                do{
-                    printf("\nEntre com o código do livro: ");
-                        scanf("%d",&codigo);
-                    printf("\nESCOLHA A ÀREA DO LIVRO\n");
-                    printf("\n1/P Ciências Exatas");
-                    printf("\n2/P Ciências Humanas");
-                    printf("\n3/P Ciências Biomedicas\n0/P Voltar para o menu anteriso: ");
-                        scanf("%d",&area);
-                    switch(area){
-                        case 0:
-                            printf("\nAté mais");
-                        break;
-                        case 1:
-                            buscarLivros(cienciasExatas,codigo);
-                        break;
-                        case 2:
-                            buscarLivros(cienciasHumanas,codigo);
-                        break;
-                        case 3:
-                            buscarLivros(cienciasBiomedicas,codigo);
-                        break;
-                        default:
-                            printf("\nDados de área invalidos.");
-                            printf("\nDigite novamente.");
-                    }
-                }while(area!=0);
+                //Busca por código
+                    do{
+                        printf("\nESCOLHA UMA OPÇÂO\n");
+                        printf("\n1/P buscar um livro");
+                        printf("\n2/P voltar para o menu anterior: ");
+                            scanf("%d",&opcao);
+                            fflush(stdin);
+                        switch(opcao){
+
+                            case 1:
+                                do{
+                                    printf("\nEntre com o código do livro: ");
+                                        scanf("%d",&codigo);
+                                        fflush(stdin);
+                                    printf("\nESCOLHA A ÀREA DO LIVRO\n");
+                                    printf("\n1/P Ciências Exatas");
+                                    printf("\n2/P Ciências Humanas");
+                                    printf("\n3/P Ciências Biomedicas\n4/P Voltar para o menu anterior: ");
+                                        scanf("%d",&area);
+                                        fflush(stdin);
+                                    switch(area){
+                                        case 1:
+                                            area=buscarLivros(cienciasExatas,codigo);
+                                        break;
+                                        case 2:
+                                            area=buscarLivros(cienciasHumanas,codigo);
+                                        break;
+                                        case 3:
+                                            area=buscarLivros(cienciasBiomedicas,codigo);
+                                        break;
+                                        case 4:
+                                            printf("Ok!!!");
+                                        break;
+                                        default:
+                                            printf("\nDados de área invalidos.");
+                                            printf("\nDigite novamente.");
+                                    }
+                                }while(area!=4);
+                            break;
+                            case 2:
+                                printf("Ok!!!");
+                            break;
+                            default :
+                                printf("\nEntrada invalida!");
+                        }
+                    }while(opcao!=2);
+
+            break;
+            case 2:
+                //Busca por nome
+                    do{
+                        printf("\nESCOLHA UMA OPÇÂO\n");
+                        printf("\n1/P buscar um livro");
+                        printf("\n2/P voltar para o menu anterior: ");
+                            scanf("%d",&opcao);
+                            fflush(stdin);
+                        switch(opcao){
+
+                            case 1:
+                                do{
+                                    printf("\nEntre com o nome do livro: ");
+                                        scanf("%[^\n]",&nome);
+                                        fflush(stdin);
+                                    printf("\nESCOLHA A ÀREA DO LIVRO\n");
+                                    printf("\n1/P Ciências Exatas");
+                                    printf("\n2/P Ciências Humanas");
+                                    printf("\n3/P Ciências Biomedicas\n4/P Voltar para o menu anterior: ");
+                                        scanf("%d",&area);
+                                        fflush(stdin);
+                                    switch(area){
+                                        case 1:
+                                            area=buscarLivrosPorNome(cienciasExatas,nome);
+                                        break;
+                                        case 2:
+                                            area=buscarLivrosPorNome(cienciasHumanas,nome);
+                                        break;
+                                        case 3:
+                                            area=buscarLivrosPorNome(cienciasBiomedicas,nome);
+                                        break;
+                                        case 4:
+                                            printf("Ok!!!");
+                                        break;
+                                        default:
+                                            printf("\nDados de área invalidos.");
+                                            printf("\nDigite novamente.");
+                                    }
+                                }while(area!=4);
+                            break;
+                            case 2:
+                                printf("Ok!!!");
+                            break;
+                            default :
+                                printf("\nEntrada invalida!");
+                        }
+                    }while(opcao!=2);
+
+            break;
+            case 10:
+                printf("\nAté logo");
             break;
             default :
                 printf("\nEntrada invalida!");
         }
-    }while(opcao!=0);
-
+    }while(opcao!=10);
 
     return 0;
 }
-
-void buscarLivros(Obras livros[TAM_MAX], int codigoDeBusca){
+int buscarLivrosPorNome(Obras livros[TAM_MAX], char nomeDoLivrosDeBusca[40]){
     int controle = 0;
     for(int i = 0; i < TAM_MAX; i++){
-        if(livros[i].codigoDeCatalogacao==codigoDeBusca){
+        if((strcmp(livros[i].nomeDaObra, nomeDoLivrosDeBusca))==0){
             printf("\nNome do livros: %s",livros[i].nomeDaObra);
-            printf("\nNome do autor: %s",livros[i].nomeDoAutor);
+            printf("\nNome do autor: %s\n",livros[i].nomeDoAutor);
             controle = 1;
+            return 4;
         }
     }
     if(controle==0){
         printf("\nLivro não encontrado.");
     }
-
+}
+int buscarLivros(Obras livros[TAM_MAX], int codigoDeBusca){
+    int controle = 0;
+    for(int i = 0; i < TAM_MAX; i++){
+        if(livros[i].codigoDeCatalogacao==codigoDeBusca){
+            printf("\nNome do livros: %s",livros[i].nomeDaObra);
+            printf("\nNome do autor: %s\n",livros[i].nomeDoAutor);
+            controle = 1;
+            return 4;
+        }
+    }
+    if(controle==0){
+        printf("\nLivro não encontrado.");
+    }
 }
 
 void bubblesort(Obras livros[TAM_MAX]){
