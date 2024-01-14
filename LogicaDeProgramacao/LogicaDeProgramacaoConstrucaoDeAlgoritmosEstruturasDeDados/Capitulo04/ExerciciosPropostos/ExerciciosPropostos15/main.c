0feee67889c53e10d9851b5dfd4291bd7a94ad23
Wallace Petrik <wallacepetrik@gmail.com>
2024-01-14 14:17:07 -0300

    Estruturas de Dados Heterogêneas - Registros
    Capítulo 4, Exercício proposto 15f

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos15/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos15/main.c
@@ -5,8 +5,6 @@
 
 #define TAM_MAX 3
 
-
-
 typedef struct{
     int codigoDeCatalogacao;
     int numeroDePaginas;
@@ -16,12 +14,11 @@ typedef struct{
     char doado[40];
 }Obras;
 
-
 void bubblesort(Obras livros[TMP_MAX]);
 int buscarLivros(Obras livros[TAM_MAX], int codigoDeBusca);
 int buscarLivrosPorNome(Obras livros[TAM_MAX], char nomeDoLivrosDeBusca[40]);
 int impremi(Obras livros[TAM_MAX],int number);
-
+int alterarLivro(Obras livros[TAM_MAX], int cod);
 int main(){
     setlocale(LC_ALL,"");
 
@@ -37,34 +34,39 @@ int main(){
     bubblesort(cienciasHumanas);
 
         do{
+        system("cls");
         printf("\nESCOLHA UMA OPÇÂO\n");
-        printf("\n1/P buscar um livro código");
-        printf("\n2/P buscar um livro nome");
-        printf("\n3/P listar todos os livros doados.");
-        printf("\n4/P listar todos os livros comprados.");
-        printf("\n10/P Sair: ");
+        printf("\n1/Para buscar um livro código");
+        printf("\n2/Para buscar um livro nome");
+        printf("\n3/Para listar todos os livros doados.");
+        printf("\n4/Para listar todos os livros comprados.");
+        printf("\n5/Para alterar dados do livro.");
+        printf("\n6/Para excluir livro.");
+        printf("\n10/Para Sair: ");
             scanf("%d",&opcao);
 
         switch(opcao){
             case 1:
-                //Busca por código
                     do{
+                         system("cls");
                         printf("\nESCOLHA UMA OPÇÂO\n");
-                        printf("\n1/P buscar um livro");
-                        printf("\n2/P voltar para o menu anterior: ");
+                        printf("\n1/Para buscar um livro");
+                        printf("\n2/Para voltar: ");
                             scanf("%d",&opcao);
                             fflush(stdin);
                         switch(opcao){
 
                             case 1:
                                 do{
+                                    system("cls");
                                     printf("\nEntre com o código do livro: ");
                                         scanf("%d",&codigo);
                                         fflush(stdin);
+                                        system("cls");
                                     printf("\nESCOLHA A ÀREA DO LIVRO\n");
-                                    printf("\n1/P Ciências Exatas");
-                                    printf("\n2/P Ciências Humanas");
-                                    printf("\n3/P Ciências Biomedicas\n4/P Voltar para o menu anterior: ");
+                                    printf("\n1/Para Ciências Exatas");
+                                    printf("\n2/Para Ciências Humanas");
+                                    printf("\n3/Para Ciências Biomedicas\n4/Para Voltar para o menu anterior: ");
                                         scanf("%d",&area);
                                         fflush(stdin);
                                     switch(area){
@@ -96,24 +98,26 @@ int main(){
 
             break;
             case 2:
-                //Busca por nome
                     do{
+                        system("cls");
                         printf("\nESCOLHA UMA OPÇÂO\n");
-                        printf("\n1/P buscar um livro");
-                        printf("\n2/P voltar para o menu anterior: ");
+                        printf("\n1/Para buscar um livro");
+                        printf("\n2/Para voltar para o menu anterior: ");
                             scanf("%d",&opcao);
                             fflush(stdin);
                         switch(opcao){
 
                             case 1:
                                 do{
+                                    system("cls");
                                     printf("\nEntre com o nome do livro: ");
                                         scanf("%[^\n]",&nome);
                                         fflush(stdin);
+                                        system("cls");
                                     printf("\nESCOLHA A ÀREA DO LIVRO\n");
-                                    printf("\n1/P Ciências Exatas");
-                                    printf("\n2/P Ciências Humanas");
-                                    printf("\n3/P Ciências Biomedicas\n4/P Voltar para o menu anterior: ");
+                                    printf("\n1/Para Ciências Exatas");
+                                    printf("\n2/Para Ciências Humanas");
+                                    printf("\n3/Para Ciências Biomedicas\n4/Para Voltar para o menu anterior: ");
                                         scanf("%d",&area);
                                         fflush(stdin);
                                     switch(area){
@@ -145,21 +149,88 @@ int main(){
 
             break;
             case 3:
-                //Livros doados
+                system("cls");
                 printf("\nLivros doados\n");
                 totaDeLivrosDoados=impremi(cienciasHumanas,opcao);
                 totaDeLivrosDoados+=impremi(cienciasExatas,opcao);
                 totaDeLivrosDoados+=impremi(cienciasBiomedicas,opcao);
-                printf("\n\nTotal de livros doados = %d",totaDeLivrosDoados);
+                printf("\n\nTotal de livros doados = %d\n\n",totaDeLivrosDoados);
+                system("pause");
             break;
             case 4:
-                //Livros comprados
+                system("cls");
                 totaDeLivrosDoados = 0;
-0;              printf("\nLivros comprados\n");
+                printf("\nLivros comprados\n");
                 totaDeLivrosDoados=impremi(cienciasHumanas,opcao);
                 totaDeLivrosDoados+=impremi(cienciasExatas,opcao);
                 totaDeLivrosDoados+=impremi(cienciasBiomedicas,opcao);
-                printf("\n\nTotal de livros comprados = %d",totaDeLivrosDoados);
+                printf("\n\nTotal de livros comprados = %d\n\n",totaDeLivrosDoados);
+                system("pause");
+            break;
+            case 5:
+                system("cls");
+                do{
+                    printf("\nEntre com o código do livro que deseja alterar: ");
+                        scanf("%d",&codigo);
+                        system("cls");
+                    printf("\nESCOLHA A ÀREA DO LIVRO\n");
+                    printf("\n1/Para Ciências Exatas");
+                    printf("\n2/Para Ciências Humanas");
+                    printf("\n3/Para Ciências Biomedicas\n4/Para Voltar para o menu anterior: ");
+                        scanf("%d",&area);
+                    switch(area){
+                        case 1:
+                            area =  alterarLivro(cienciasExatas,codigo);
+                        break;
+                        case 2:
+                            area =  alterarLivro(cienciasHumanas,codigo);
+                        break;
+                        case 3:
+                            area =  alterarLivro(cienciasBiomedicas,codigo);
+                        break;
+                        case 4:
+                            printf("Ok!!!");
+                        break;
+                        default:
+                            printf("\nDados de área invalidos.");
+                            printf("\nDigite novamente.");
+                    }
+                }while(area!=4);
+
+            break;
+            case 6:
+                //Excluir livro
+                do{
+                    system("cls");
+                    printf("\nDigite o código que deseja excluir:");
+                        scanf("%d",&codigo);
+                    printf("\n1/Para Ciências Exatas");
+                    printf("\n2/Para Ciências Humanas");
+                    printf("\n3/Para Ciências Biomedicas\n4/Para Voltar para o menu anterior: ");
+                    printf("\nDigite a áre do livro:");
+                        scanf("%d",&area);
+
+                    switch(area){
+                        case 1:
+                            //Criar função para excluir
+                        break;
+                        case 2:
+
+                        break;
+                        case 3:
+
+                        break;
+                        case 4:
+                            system("cls");
+                            printf("\nOk!!!\n\n");
+                            system("pause");
+                        break;
+                        default:
+                            system("cls");
+                            printf("\nDados invalidos.\n");
+                            system("pause");
+                    }
+                }while(area<1 && area>4);
             break;
             case 10:
                 printf("\nAté logo");
@@ -172,17 +243,22 @@ int main(){
     return 0;
 }
 int buscarLivrosPorNome(Obras livros[TAM_MAX], char nomeDoLivrosDeBusca[40]){
+    system("cls");
     int controle = 0;
     for(int i = 0; i < TAM_MAX; i++){
         if((strcmp(livros[i].nomeDaObra, nomeDoLivrosDeBusca))==0){
+
             printf("\nNome do livros: %s",livros[i].nomeDaObra);
-            printf("\nNome do autor: %s\n",livros[i].nomeDoAutor);
+            printf("\nNome do autor: %s\n\n",livros[i].nomeDoAutor);
             controle = 1;
+            system("pause");
             return 4;
         }
     }
     if(controle==0){
-        printf("\nLivro não encontrado.");
+        printf("\nLivro não encontrado.\n\n");
+        system("pause");
+        return 4;
     }
 }
 
@@ -203,17 +279,22 @@ int impremi(Obras livros[TAM_MAX],int number){
 }
 
 int buscarLivros(Obras livros[TAM_MAX], int codigoDeBusca){
+    system("cls");
     int controle = 0;
     for(int i = 0; i < TAM_MAX; i++){
         if(livros[i].codigoDeCatalogacao==codigoDeBusca){
             printf("\nNome do livros: %s",livros[i].nomeDaObra);
-            printf("\nNome do autor: %s\n",livros[i].nomeDoAutor);
+            printf("\nNome do autor: %s\n\n",livros[i].nomeDoAutor);
             controle = 1;
+            system("pause");
             return 4;
+
         }
     }
     if(controle==0){
-        printf("\nLivro não encontrado.");
+        printf("\nLivro não encontrado.\n\n");
+        system("pause");
+        return 4;
     }
 }
 
@@ -233,3 +314,67 @@ void bubblesort(Obras livros[TAM_MAX]){
         fim --;
     }while(controle!=0);
 }
+
+int alterarLivro(Obras livros[TAM_MAX], int cod){
+    system("cls");
+    int controle = 0, op;
+    for(int i = 0; i < TAM_MAX; i++){
+        if(livros[i].codigoDeCatalogacao==cod){
+            printf("\nNome do livros: %s",livros[i].nomeDaObra);
+            printf("\nNome do autor: %s",livros[i].nomeDoAutor);
+            printf("\nCódigo de catalogação: %d",livros[i].codigoDeCatalogacao);
+            printf("\nEditora: %s",livros[i].editora);
+            printf("\nDoado: %s",livros[i].doado);
+            printf("\nNº de páginas: %d\n",livros[i].numeroDePaginas);
+
+            printf("\n1/p alterar o nome do livro.");
+            printf("\n2/p alterar o nome do autor.");
+            printf("\n3/p alterar o código do livro.");
+            printf("\n4/p alterar a editora do livro.");
+            printf("\n5/p alterar doado ou não.");
+            printf("\n6/p alterar número de página.");
+            printf("\n7/p voltar.");
+                scanf("%d",&op);
+                fflush(stdin);
+            switch(op){
+                case 1:
+                    printf("\nDigite o nome do livros: ");
+                        scanf("%[^\n]",&livros[i].nomeDaObra);
+                break;
+                case 2:
+                    printf("\nDigite o nome do autor: ");
+                        scanf("%[^\n]",&livros[i].nomeDoAutor);
+                break;
+                case 3:
+                    printf("\nDigite o código do livros: ");
+                        scanf("%d",&livros[i].codigoDeCatalogacao);
+                break;
+                case 4:
+                    printf("\nDigite o nome da editora: ");
+                        scanf("%[^\n]",&livros[i].editora);
+                break;
+                case 5:
+                    printf("\nDigite Sim ou Não para doado: ");
+                        scanf("%[^\n]",&livros[i].doado);
+                break;
+                case 6:
+                    printf("\nDigite o número de paginas: ");
+                        scanf("%[^\n]",&livros[i].numeroDePaginas);
+                break;
+                case 7:
+                    return 4;
+                break;
+                default:
+                    printf("\nOpção invalida.");
+            }
+
+            controle = 1;
+            return 4;
+        }
+    }
+    if(controle==0){
+        printf("\nLivro não encontrado.");
+    }
+}
+
+
