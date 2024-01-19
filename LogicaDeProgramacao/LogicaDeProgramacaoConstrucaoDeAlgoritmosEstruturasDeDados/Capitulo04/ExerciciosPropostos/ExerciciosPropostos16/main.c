25bddb0700b70b08ef15c8f8625dc7ca67a2aaa2
Wallace Petrik <wallacepetrik@gmail.com>
2024-01-19 14:21:33 -0300

    Estruturas de Dados Heterogêneas - Registros
    Capítulo 4, Exercício proposto 16b

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos16/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos16/main.c
@@ -7,7 +7,7 @@
 typedef struct{
     char proprietario[40];
     char modelo[40];
-    char placa[8];
+    char placa[9];
     char cor[40];
     int combustivel;
     int chassi;
@@ -17,13 +17,14 @@ typedef struct{
 int main(){
     setlocale(LC_ALL,"");
 
-    Veiculo carros[Tam_Max] = {"Wallace","Astra","WPS-0805","Branco",3,123321,2014};
+    Veiculo carros[Tam_Max] = {"Wallace","Astra","APS-0805","Prata",3,123321,2014};
 
-    int controle = 0, contador = 0,opcao;
+    int controle = 0, contador = 0, opcao;
 
     do{
         printf("Escolha uma opção\n");
-        printf("\n1/p Listar proprietários de carros com anos posterios 1980 movidos a diesel.");
+        printf("\n1/Para listar proprietários de carros com anos posterios 1980 movidos a diesel.");
+        printf("\n2/Para listar todas as placas que comecem com a letra A e termina com 0, 2, 4 ou 7");
         printf("\nDigite uma opção: ");
             scanf("%d",&opcao);
         switch(opcao){
@@ -40,6 +41,24 @@ int main(){
                 }
                 if(controle==0){
                     printf("Nenhum proprietário encontrado\n");
+                }
+                system("pause");
+                system("cls");
+            break;
+            case 2:
+                system("cls");
+                for(int i = 0; i < Tam_Max; i++){
+                    if((carros[i].placa[0]=='a'||carros[i].placa[0]=='A')&&
+                       (carros[i].placa[7]=='0'||carros[i].placa[7]=='2'||
+                        carros[i].placa[7]=='4'||carros[i].placa[7]=='7')){
+
+                        printf("Proprietário = %s\n",carros[i].proprietario);
+                        printf("Placa = %s \n",carros[i].placa);
+                        controle = 1;
+                    }
+                }
+                if(controle==0){
+                    printf("Nenhum placa encontrado\n");
                 }
                 system("pause");
                 system("cls");
