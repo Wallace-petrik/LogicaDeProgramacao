e60289e558811c11fe92829894d556dca9acb9ee
Wallace Petrik <wallacepetrik@gmail.com>
2024-01-22 07:38:02 -0300

    Estruturas de Dados Heterogêneas - Registros
    Capítulo 4, Exercício proposto 17b

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos17/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos17/main.c
@@ -20,7 +20,7 @@ typedef struct{
 int main(){
     setlocale(LC_ALL,"");
 
-    Linha onibus[10] = {"Campinas","Rio de Janeiro","20/03/2024","10:30",400,01,"Wallace",1};
+    Linha onibus[10] = {{"Campinas","Rio de Janeiro","20/03/2024","10:30",400,01,"Wallace",1,02,"Ellen",2},{"SP","Minas","25/03/2024","12:45",350,02,"Ellen",2}};
 
     int opcao = 0, linhasDoOnibus = 0, controle = 0,contador = 0;
 
@@ -33,6 +33,7 @@ int main(){
         switch(opcao){
 
             case 1:
+                controle = 0;
                 system("cls");
                 printf("Entre com o numero do ônibus:");
                     scanf("%d",&linhasDoOnibus);
@@ -52,9 +53,24 @@ int main(){
                 system("cls");
             break;
             case 2:
-
-
-
+                controle = 0;
+                for(int i = 0; i < 10; i++){
+                    for(int j = 0; j < 44; j++){
+                        if(onibus[i].passageiros[j].sexo==1||onibus[i].passageiros[j].sexo==2){
+                            contador++;
+                        }
+                    }
+                    if(contador==44){
+                        printf("Linha de ônibus %d lotada.\n",i);
+                        controle = 1;
+                    }
+                    contador = 0;
+                }
+                if(controle==0){
+                    printf("Nenhum ônibus lotado.\n");
+                }
+                system("pause");
+                system("cls");
             break;
             case 10:
                 system("cls");
