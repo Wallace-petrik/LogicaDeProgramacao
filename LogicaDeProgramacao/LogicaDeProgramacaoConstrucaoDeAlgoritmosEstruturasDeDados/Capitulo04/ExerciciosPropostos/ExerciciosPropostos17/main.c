3ffe38fe0e341e7b96b3fd38a8fd0ead46972306
Wallace Petrik <wallacepetrik@gmail.com>
2024-01-22 11:22:58 -0300

    Estruturas de Dados Heterogêneas - Registros
    Capítulo 4, Exercício proposto 17c

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos17/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos17/main.c
@@ -12,7 +12,7 @@ typedef struct{
     char origem[40];
     char destino[40];
     char data[11];
-    char horario[6];
+    int horario[2];
     int distancia;
     Poltronas passageiros[44];
 }Linha;
@@ -20,14 +20,17 @@ typedef struct{
 int main(){
     setlocale(LC_ALL,"");
 
-    Linha onibus[10] = {{"Campinas","Rio de Janeiro","20/03/2024","10:30",400,01,"Wallace",1,02,"Ellen",2},{"SP","Minas","25/03/2024","12:45",350,02,"Ellen",2}};
+    Linha onibus[10] = {{"Campinas","Rio de Janeiro","20/03/2024",17,30,400,01,"Wallace",1}};
 
-    int opcao = 0, linhasDoOnibus = 0, controle = 0,contador = 0;
+    int opcao = 0, linhasDoOnibus = 0, controle = 0,contador = 0,horas = 0, minutos = 0, horasChegada = 0, minutosChegada = 0;
+    float tempo = 0,chegada;
+    const int velocidadeM = 60;
 
     do{
         system("cls");
         printf("1 Para buscar ônibus.\n");
         printf("2 Para linha de ônibus que estão lotadas.\n");
+        printf("3 Para informa tempo estimado de chegada e duração da viagem.\n");
         printf("10 Para sair\n");
             scanf("%d",&opcao);
         switch(opcao){
@@ -39,7 +42,7 @@ int main(){
                     scanf("%d",&linhasDoOnibus);
                 for(int i = 0; i < 10; i++){
                     if(i==linhasDoOnibus){
-                        printf("Horário de Saída : %s\n",onibus[i].horario);
+                        printf("Horário de Saída : %d:%d\n",onibus[i].horario[0],onibus[i].horario[1]);
                         printf("Distância = %d\n",onibus[i].distancia);
                         controle = 1;
                         break;
@@ -53,6 +56,7 @@ int main(){
                 system("cls");
             break;
             case 2:
+                system("cls");
                 controle = 0;
                 for(int i = 0; i < 10; i++){
                     for(int j = 0; j < 44; j++){
@@ -69,6 +73,42 @@ int main(){
                 if(controle==0){
                     printf("Nenhum ônibus lotado.\n");
                 }
+                system("pause");
+                system("cls");
+            break;
+            case 3:
+                system("cls");
+                controle = 0;
+                do{
+                    printf("Entre com a linha do ônibus: ");
+                        scanf("%d",&controle);
+
+                    if(controle>=0&&controle<=9){
+                        tempo = (float)onibus[controle].distancia/velocidadeM;
+                        horas = tempo;
+                        minutos = (tempo-horas)*60;
+
+                        if(onibus[controle].horario[1]+minutos>=60){
+                            minutosChegada = ((minutos+onibus[controle].horario[1])%60);
+                            horasChegada = (horas+onibus[controle].horario[0]+1);
+                        }else{
+                            minutosChegada = minutos+onibus[controle].horario[1];
+                        }
+
+                        if(horasChegada>24){
+                            horasChegada = horasChegada%24;
+                        }else if(horasChegada==24){
+                            horasChegada = 0;
+                        }
+
+                        printf("A viagem da linha %d terá duração de %d:%d com tempo estimado para ás %.2d:%.2d horas.\n",
+                               controle,(int)horas,(int)minutos,horasChegada,minutosChegada);
+                    }else{
+                        printf("\nLinha não encontrada.\n");
+                        system("pause");
+                        system("cls");
+                    }
+                }while(controle<0&&controle>9);
                 system("pause");
                 system("cls");
             break;
