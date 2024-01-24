515922a4a42c83d5beb11f9b4af918fdca54a7bb
Wallace Petrik <wallacepetrik@gmail.com>
2024-01-24 06:04:29 -0300

    Estruturas de Dados Heterogêneas - Registros
    Capítulo 4, Exercício proposto 18.1

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos18/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo04/ExerciciosPropostos/ExerciciosPropostos18/main.c
@@ -21,7 +21,34 @@ int main(){
 
     Alunos aluno[tam] = {112233,"Wallace",7,20241,"M",8.9,"S",1994,"SP"};
 
+    int opcao;
 
+    do{
+        system("cls");
+        printf(" 1 Para porcentagem de aprovados e reprovados.\n");
+        printf(" 9 Para sair.\n");
+        printf(" Escolha uma opção: ");
+            scanf("%d",&opcao);
+
+        switch(opcao){
+            case 1:
+
+
+
+            break;
+            case 9:
+                system("cls");
+                printf("Até logo!\n");
+            break;
+            default :
+                system("cls");
+                printf("Opção inválida\n");
+                system("pause");
+        }
+
+    }while(opcao!=9);
 
     return 0;
 }
+
+void ordenarDados (Alunos alunoParaOrdenar, )
