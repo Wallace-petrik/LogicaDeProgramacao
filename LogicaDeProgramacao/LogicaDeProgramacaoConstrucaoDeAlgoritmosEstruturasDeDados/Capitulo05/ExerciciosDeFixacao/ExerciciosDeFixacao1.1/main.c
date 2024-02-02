b63c2b0061a34f6bc5801f45a8ca187fbe062186
Wallace Petrik <wallacepetrik@gmail.com>
2024-02-02 16:07:19 -0300

    Arquivos - Aplicação de arquivos
    Exercício de fixação 1.1 incompleto 2.0

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosDeFixacao/ExerciciosDeFixacao1.1/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosDeFixacao/ExerciciosDeFixacao1.1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 
 typedef struct{
     int codLivro;
@@ -15,28 +16,80 @@ typedef struct{
 int main(){
     setlocale(LC_ALL,"");
 
-    Livro livro;
-
     FILE *arquivo;
 
-    int opcao;
+    Livro livro,aux;
 
-    if((arquivo=fopen("banco","a+b"))==NULL){
-        printf("Problema na criação do arquivo!\n");
-        system("pause");
-        exit(0);
-    }
+    int opcao,controle;
 
     do{
         printf("Escolha uma opção\n\n");
-        printf("1 Para buscar livros por assunto\n");
+        printf("1 Para adicionar um livros\n");
+        printf("2 Para buscar livros por assunto\n");
         printf("3 Para sair\n");
             scanf("%d",&opcao);
 
         switch(opcao){
             case 1:
                 system("cls");
+                arquivo = fopen("arquivo","a+b");
+                if((arquivo ==NULL)){
+                    printf("Erro na abertura do arquivo!!!\n");
+                }else{
+                    printf("\nEntre com os dados do livro!\n\n");
+                    printf("Código: ");
+                        scanf("%d%*c",&livro.codLivro);
+                    printf("Título: ");
+                        fgets(livro.titulo,30,stdin);
+                        livro.titulo[strcspn(livro.titulo,"\n")] = '\0';
+                    printf("Autor: ");
+                        fgets(livro.autor,30,stdin);
+                        livro.autor[strcspn(livro.autor,"\n")] = '\0';
+                    printf("Assunto: ");
+                        fgets(livro.assunto,30,stdin);
+                        livro.assunto[strcspn(livro.assunto,"\n")] = '\0';
+                    printf("Editora: ");
+                        fgets(livro.editora,30,stdin);
+                        livro.editora[strcspn(livro.editora,"\n")] = '\0';
+                    printf("Ano: ");
+                        scanf("%d%*c",&livro.ano);
+                    printf("Edição: ");
+                        scanf("%d%*c",&livro.editora);
 
+                    fwrite(&livro,sizeof(livro),1,arquivo);
+
+                    if(ferror(arquivo)){
+                        printf("Erro na gravação!\n");
+                    }else{
+                        printf("Dados gravados com sucesso!\n");
+                    }
+
+                    if(!fclose(arquivo)){
+                        printf("Arquivo fechado com sucesso!\n");
+                    }else{
+                        printf("Erro no fechamento do arquivo!\n");
+                    }
+                }
+                system("pause");
+                system("cls");
+            break;
+            case 2:
+                system("cls");
+                printf("Sobre qual assunto está buscando?");
+                    scanf("%s",&aux.assunto);
+
+            while(!feof(arquivo)){
+                controle = fread(&livro,sizeof(Livro),1,arquivo);
+                if(ferror(arquivo)){
+                    printf("Erro na leitura do arquivo");
+                }else{
+                    if(strcmp(aux.assunto,livro.assunto)){
+                        printf("Nome do livro: %s",livro.titulo);
+                        printf("Nome do autor: %s",livro.autor);
+                    }
+                }
+
+            }
 
             break;
             case 3:
