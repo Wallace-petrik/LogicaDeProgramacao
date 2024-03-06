73b29f4fa7961e1b516ec2483cf397a4e7071945
Wallace Petrik <wallacepetrik@gmail.com>
2024-03-05 21:31:08 -0300

    Arquivos - Exercício proposto
    Exercício de proposto 03b.1

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos03/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos03/main.c
@@ -37,17 +37,23 @@ typedef struct{
 
 }Lancamento;
 
+typedef struct{
+    int ag;
+    float saldoAg;
+}Saldo;
+
 int main(){
 
     setlocale(LC_ALL,"");
 
     int opcao,controle,contador;
 
-    FILE *arqCliente, *arqAgenciaBancaria, *arqTansacao;
+    FILE *arqCliente, *arqAgenciaBancaria, *arqTansacao, *arqRanking;
 
     Correntistas cliente;
     Agencia agenciaBancaria,agenciaBancariaAux;
     Lancamento transacao;
+    Saldo saldoAgencia,saldoAgenciaAux1,saldoAgenciaAux2;
 
     time_t mytime;
     mytime = time(NULL);
@@ -58,6 +64,7 @@ int main(){
         system("cls");
         printf("1 para abrir conta.\n");
         printf("2 para listar quantidade de correntistas de cada agência.\n");
+        printf("3 para listar ranking das agências com maior saldo.\n");
         printf("0 para sair!\n");
             scanf("%d",&opcao);
 
@@ -160,10 +167,74 @@ int main(){
                 }
 
             }
+
             if(fclose(arqAgenciaBancaria)==0 && fclose(arqCliente)==0 && fclose(arqTansacao)==0){
                 printf("Sucesso na execução!!!\n");
                 system("pause");
             }
+
+        break;
+        case 3:
+
+            system("cls");
+            if((arqRanking = fopen("arqRanking.dat","w+"))==NULL ||
+              (arqAgenciaBancaria = fopen("arqAgenciaBancaria.dat","rb"))==NULL ||
+              (arqCliente = fopen("arqCliente.dat","rb"))==NULL ||
+              (arqTansacao = fopen("arqTansacao.dat","rb")) == NULL){
+
+                printf("Erro ao abrir os arquivos!!!\n");
+
+                exit(1);
+            }else{
+
+                contador = 0;
+                while(fread(&agenciaBancariaAux,sizeof(agenciaBancariaAux),1,arqAgenciaBancaria)){
+
+                    saldoAgencia.saldoAg = 0;
+
+                    while(fread(&transacao,sizeof(transacao),1,arqTansacao)){
+
+                        if(transacao.contaCorrente==(agenciaBancariaAux.numeroAgencia*100)+agenciaBancariaAux.numeroAgencia){
+
+                            saldoAgencia.saldoAg += transacao.valor;
+
+                        }
+                    }
+
+                    saldoAgencia.ag = agenciaBancariaAux.numeroAgencia;
+                    fwrite(&saldoAgencia,sizeof(saldoAgencia),1,arqRanking);
+                    fseek(arqTansacao,0,SEEK_SET);
+                    contador++;
+                }
+
+                do{
+                    controle = 0;
+
+                    for(int i = 0; i < contador - 1; i++){
+
+                        fseek(arqRanking,i*sizeof(saldoAgenciaAux1),SEEK_SET);
+                        fread(&saldoAgenciaAux1,sizeof(saldoAgenciaAux1),1,arqRanking);
+                        fseek(arqRanking,(i+1)*sizeof(saldoAgenciaAux2),SEEK_SET);
+                        fread(&saldoAgenciaAux2,sizeof(saldoAgenciaAux2),1,arqRanking);
+
+                        /*if(){
+
+
+
+                           controle = i;
+                        }*/
+                    }
+                    contador--;
+                }while(controle!=0);
+
+            }
+
+            if(fclose(arqAgenciaBancaria)==0 && fclose(arqCliente)==0 && fclose(arqTansacao)==0){
+                printf("Sucesso na execução!!!\n");
+                system("pause");
+            }
+
+
         break;
         default:
             system("cls");
