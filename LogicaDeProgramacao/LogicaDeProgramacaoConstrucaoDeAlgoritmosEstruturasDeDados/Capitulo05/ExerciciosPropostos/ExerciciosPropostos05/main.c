dfc93c8dd164aabccd38a9a24efb9cdae4e5887a
Wallace Petrik <wallacepetrik@gmail.com>
2024-04-09 05:53:39 -0300

    Arquivos - Exercício proposto
    Exercício proposto 05 completo

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos05/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos05/main.c
@@ -19,12 +19,13 @@ int main(){
 
     Alunos aluno, alunoAux;
 
-    int opcao = 0, controle =0;
-
+    int opcao = 0, controle =0, qualNota;
+    float nota = 0;
     do{
         system("cls");
         printf("1 para registrar aluno\n");
         printf("2 para buscar aluno\n");
+        printf("3 para alterar dados\n");
         printf("\nDigite uma opção: ");
             scanf("%d",&opcao);
             fflush(stdin);
@@ -89,6 +90,56 @@ int main(){
                         printf("\nAluno não encontrado\n");
                     }
                     system("pause");
+                }
+
+            break;
+            case 3:
+
+                if((arqDiario = fopen("arqDiario.txt","r+b"))==NULL){
+                    printf("Erro ao abrir o arquivo!");
+                    exit(1);
+                }else{
+
+                    controle = 0;
+                    printf("Entre com o número do aluno: ");
+                        scanf("%d",&alunoAux.numero);
+                    printf("Qual nota deseja alterar: ");
+                        scanf("%d",&qualNota);
+                    while(fread(&aluno,sizeof(aluno),1,arqDiario)){
+                        if(aluno.numero==alunoAux.numero){
+                            switch(qualNota){
+                                case 1 : nota = aluno.n1; break;
+                                case 2 : nota = aluno.n2; break;
+                                case 3 : nota = aluno.n3; break;
+                                case 4 : nota = aluno.n4; break;
+                                default: printf("Opção de nota invalida\n");
+                            }
+                            printf("\n%s possui nota, %d = %.1f",aluno.nome,qualNota,nota);
+                            printf("\nDigite a nova nota: ");
+                                scanf("%f",&nota);
+                            switch(qualNota){
+                                case 1 : aluno.n1 = nota; break;
+                                case 2 : aluno.n2 = nota; break;
+                                case 3 : aluno.n3 = nota; break;
+                                case 4 : aluno.n4 = nota; break;
+                                default: printf("Opção de nota invalida\n");
+                            }
+                            fseek(arqDiario,(-1)*(sizeof(aluno)),SEEK_CUR);
+                            fwrite(&aluno,sizeof(aluno),1,arqDiario);
+                            controle = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if(fclose(arqDiario)==0){
+                    if(controle == 1){
+                        printf("Dados alterados com sucesso!\n");
+                        system("pause");
+                    }else{
+                        printf("Aluno não encontrado!\n");
+                        system("pause");
+                    }
                 }
 
             break;
