a88b06fad3b677f103849cb3bea343015afdfb39
Petrik <wallacepetrik@gmail.com>
2024-04-30 10:09:51 -0300

    Arquivos - Exercício proposto
    Exercício proposto 08a.1

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos08/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos08/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <time.h>
 
 #define tam 100
 
@@ -37,10 +38,10 @@ int main(){
 
     FILE *arqCliente, *arqAtendimento;
 
-    Cliente cliente;
+    Cliente cliente, clienteAux;
     Atendimento atendimento;
 
-    int opcao = 0;
+    int opcao = 0, controle = 0, contador = 0;
 
     if((arqAtendimento = fopen("arqAtendimento.dat","a+b")) == NULL || (arqCliente = fopen("arqCliente.dat","a+b")) ==  NULL){
         printf("Erro ao criar os arquivos!!!\n");
@@ -57,6 +58,7 @@ int main(){
          system("cls");
         printf("1 para cadastrar cliente\n");
         printf("2 para cadastrar atendimento\n");
+        printf("3 para mostrar os últimos 5 atendimentos:\n");
 
         printf("\n0 para sair\n");
 
@@ -130,6 +132,73 @@ int main(){
                 }
                 if(fclose(arqAtendimento) == 0){
                     printf("Operação realizada com sucesso!!!");
+                    system("pause");
+                }
+
+            break;
+            case 3:
+
+                if((arqCliente = fopen("arqCliente.dat","r+b")) == NULL || (arqAtendimento = fopen("arqAtendimento.dat","r+b")) == NULL){
+                    printf("Erro ao abrir os arquivos!!!");
+                    system("pause");
+                }else{
+
+                    struct tm data1 = {0};
+                    struct tm data2 = {0};
+
+                    printf("Digite o CNPJ: ");
+                        scanf("%d",&cliente.cnpj);
+
+                    fseek(arqCliente,(cliente.cnpj-1)*sizeof(cliente),SEEK_SET);
+                    fread(&clienteAux,sizeof(clienteAux),1,arqCliente);
+
+                    if(cliente.cnpj!=clienteAux.cnpj){
+                        printf("Cliente não está cadastradp\n");
+                        system("pause");
+                    }else{
+
+                        printf("Nome = %s\n",clienteAux.razaoSocial);
+
+                        fseek(arqAtendimento,0,SEEK_END);
+                        long fileSize = ftell(arqAtendimento);
+                        long qtdRegistros = fileSize / sizeof(atendimento);
+
+                        for(int i = qtdRegistros - 1; i >= 0 ; i--){
+
+                            fseek(arqAtendimento,(i * sizeof(atendimento)),SEEK_SET);
+                            fread(&atendimento,sizeof(atendimento),1,arqAtendimento);
+
+                            if(cliente.cnpj == atendimento.cnpj && atendimento.origem == 2){
+                                int dias = 0;
+                                printf("Data %d/%d/%d",atendimento.data.dia,atendimento.data.mes,atendimento.data.ano);
+
+                                if(i+1 == qtdRegistros){
+                                    data1.tm_year = atendimento.data.ano - 1900;
+                                    data1.tm_mon = atendimento.data.mes;
+                                    data1.tm_mday = atendimento.data.dia;
+                                    data2.tm_year = atendimento.data.ano - 1900;
+                                    data2.tm_mon = atendimento.data.mes;
+                                    data2.tm_mday = atendimento.data.dia;
+
+                                    time_t segundos1 = mktime(&data1);
+                                    time_t segundos2 = mktime(&data2);
+
+                                    double diferenca = difftime(segundos2, segundos1);
+
+                                    dias = diferenca / 86400;
+                                }else{
+
+
+
+                                }
+                                printf("O tempo desde o chamado anterior e de %d dias\n",dias);
+
+                            }
+                        }
+                    }
+                }
+                if(fclose(arqCliente) != 0 && fclose(arqAtendimento) != 0){
+                    printf("Erro ao finalizar operação!!!");
                     system("pause");
                 }
 
