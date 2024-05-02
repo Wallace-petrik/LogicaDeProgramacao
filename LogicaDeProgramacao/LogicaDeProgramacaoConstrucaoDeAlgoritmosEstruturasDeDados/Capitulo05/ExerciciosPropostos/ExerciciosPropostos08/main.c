d52e5012f7d05147b09f710b9147fc092e999167
Petrik <wallacepetrik@gmail.com>
2024-05-02 05:55:31 -0300

    Arquivos - Exercício proposto
    Exercício proposto 08b

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos08/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosPropostos/ExerciciosPropostos08/main.c
@@ -58,14 +58,13 @@ int main(){
          system("cls");
         printf("1 para cadastrar cliente\n");
         printf("2 para cadastrar atendimento\n");
-        printf("3 para mostrar os últimos 5 atendimentos:\n");
+        printf("3 para mostrar os últimos 5 atendimentos originados pelo ciente:\n");
+        printf("4 para mostrar os últimos 5 atendimentos originados na empresa:\n");
 
         printf("\n0 para sair\n");
 
         printf("\nDigite uma opçao: ");
             scanf("%d",&opcao);
-
-
 
         switch(opcao){
 
@@ -142,7 +141,6 @@ int main(){
                     printf("Erro ao abrir os arquivos!!!");
                     system("pause");
                 }else{
-
                     struct tm data1 = {0};
                     struct tm data2 = {0};
 
@@ -166,7 +164,7 @@ int main(){
                         long fileSize = ftell(arqAtendimento);
                         long qtdRegistros = fileSize / sizeof(atendimento);
 
-                        for(int i = qtdRegistros - 1, contador = 0; i >= 0 && contador<=5 ; i--,++contador){
+                        for(int i = qtdRegistros - 1, contador = 0; i >= 0 && contador<5 ; i--,++contador){
 
                             fseek(arqAtendimento,(i * sizeof(atendimento)),SEEK_SET);
                             fread(&atendimento,sizeof(atendimento),1,arqAtendimento);
@@ -215,6 +213,99 @@ int main(){
 
                                 printf("O tempo desde o chamado anterior é de %d dias\n",abs(dias));
 
+                            }else{
+                                contador--;
+                            }
+                        }
+                        system("pause");
+                    }
+                }
+                if(fclose(arqCliente) != 0 && fclose(arqAtendimento) != 0){
+                    printf("Erro ao finalizar operação!!!");
+                    system("pause");
+                }
+
+            break;
+            case 4:
+
+                if((arqCliente = fopen("arqCliente.dat","r+b")) == NULL || (arqAtendimento = fopen("arqAtendimento.dat","r+b")) == NULL){
+                    printf("Erro ao abrir os arquivos!!!");
+                    system("pause");
+                }else{
+                    struct tm data1 = {0};
+                    struct tm data2 = {0};
+
+                    time_t segundos1;
+                    time_t segundos2;
+
+                    printf("Digite o CNPJ: ");
+                        scanf("%d",&cliente.cnpj);
+
+                    fseek(arqCliente,(cliente.cnpj-1)*sizeof(cliente),SEEK_SET);
+                    fread(&clienteAux,sizeof(clienteAux),1,arqCliente);
+
+                    if(cliente.cnpj!=clienteAux.cnpj){
+                        printf("Cliente não está cadastradp\n");
+                        system("pause");
+                    }else{
+
+                        printf("\n\nNome = %s\n",clienteAux.razaoSocial);
+
+                        fseek(arqAtendimento,0,SEEK_END);
+                        long fileSize = ftell(arqAtendimento);
+                        long qtdRegistros = fileSize / sizeof(atendimento);
+
+                        for(int i = qtdRegistros - 1, contador = 0; i >= 0 && contador<5 ; i--,++contador){
+
+                            fseek(arqAtendimento,(i * sizeof(atendimento)),SEEK_SET);
+                            fread(&atendimento,sizeof(atendimento),1,arqAtendimento);
+
+                            if(cliente.cnpj == atendimento.cnpj && atendimento.origem == 1){
+                                int dias = 0;
+                                printf("Data %d/%d/%d\t",atendimento.data.dia,atendimento.data.mes,atendimento.data.ano);
+                                printf("Problema: %s\t",atendimento.problema);
+
+                                controle = (i == 0) ? i : i - 1;
+
+                                while(controle>=0){
+
+                                    fseek(arqAtendimento,(controle * sizeof(atendimentoAnt)),SEEK_SET);
+                                    fread(&atendimentoAnt,sizeof(atendimentoAnt),1,arqAtendimento);
+
+                                    if(cliente.cnpj == atendimentoAnt.cnpj && atendimentoAnt.origem == 1){
+                                        break;
+                                    }else{
+                                        controle--;
+                                    }
+                                }
+
+                                if(atendimentoAnt.cnpj==atendimento.cnpj){
+
+                                    data1.tm_year = atendimento.data.ano - 1900;
+                                    data1.tm_mon = atendimento.data.mes;
+                                    data1.tm_mday = atendimento.data.dia;
+                                    data2.tm_year = atendimentoAnt.data.ano - 1900;
+                                    data2.tm_mon = atendimentoAnt.data.mes;
+                                    data2.tm_mday = atendimentoAnt.data.dia;
+
+                                }else{
+
+                                    atendimentoAnt = atendimento;
+
+                                }
+                                    segundos1 = mktime(&data1);
+                                    segundos2 = mktime(&data2);
+
+                                    double diferenca = difftime(segundos2, segundos1);
+
+                                    dias = diferenca / 86400;
+
+                                    atendimentoAnt = atendimento;
+
+                                printf("O tempo desde o chamado anterior é de %d dias\n",abs(dias));
+
+                            }else{
+                                contador--;
                             }
                         }
                         system("pause");
