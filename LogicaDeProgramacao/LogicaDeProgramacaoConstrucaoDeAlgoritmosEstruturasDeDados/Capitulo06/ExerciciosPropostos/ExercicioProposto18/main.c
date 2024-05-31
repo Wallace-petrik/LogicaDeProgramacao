6506e0f3e9971db128973927c95831af40279544
Petrik <wallacepetrik@gmail.com>
2024-05-31 13:49:02 -0300

    Capítulo 6: Modularizando Algoritmos
    Exercício Proposto 18

---
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo06/ExerciciosPropostos/ExercicioProposto18/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    long long numero;
+    printf("Digite um número (até 12 dígitos): ");
+    scanf("%lld", &numero);
+    //imprimirPorExtenso(numero);
+    return 0;
+}
