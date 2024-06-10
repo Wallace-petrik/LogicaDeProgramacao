98bc7a84d350138ed9c86e75d4f40657e2e2e315
Petrik <wallacepetrik@gmail.com>
2024-06-10 09:10:36 -0300

    Capítulo 6: Modularizando Algoritmos
    Exercício Proposto 19.1

---
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo06/ExerciciosPropostos/ExercicioProposto19/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+typedef struct{
+    int numeroDoCheque;
+    int agencia;
+    int conta;
+    int dv;
+    char nome[10];
+    float valor;
+}Cheque;
+
+FILE *arqCheque;
+Cheque cheque;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+
+    leituraDosCheques();
+
+
+    return 0;
+}
+
+void leituraDosCheques(){
+    if((arqCheque = fopen("arquivo.dat","a+b"))==NULL){
+       printf("Erro ao abrir o arquivo!!!\n");
+       system("pause");
+       exit(1);
+    }else{
+        printf("Entre com os dados do cheque\n");
+        printf("Número do cheque: ");
+            scanf("%d",&cheque.numeroDoCheque);
+        printf("Agência: ");
+            scanf("%d",&cheque.agencia);
+        printf("Conta: ");
+            scanf("%d",&cheque.conta);
+        printf("DV: ");
+            scanf("%d",&cheque.dv);
+        printf("Nome: ");
+            fflush(stdin);
+            scanf("%[^\n]",&cheque.nome);
+        printf("Valor: ");
+            scanf("%f",&cheque.valor);
+
+        fwrite(&cheque,1,sizeof(cheque),arqCheque);
+    }
+    if(fclose(arqCheque)!= 0){
+        printf("Erro ao fechar o arquivo!!!");
+        exit(1);
+    }
+}
+
+
