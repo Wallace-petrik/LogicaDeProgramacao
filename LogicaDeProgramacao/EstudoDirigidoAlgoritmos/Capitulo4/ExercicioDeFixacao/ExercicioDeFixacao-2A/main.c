8cdd4b1c5a65d47dedab82531c81ea5da8acd3be
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2024-11-21 05:13:03 -0300

    Capítulo 4,  exercício 2A, Ordenar 3 valores

--- LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo4/ExercicioDeFixacao/ExercicioDeFixacao-2A/main.c
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo4/ExercicioDeFixacao/ExercicioDeFixacao-2A/main.c
@@ -4,8 +4,29 @@
 int main(){
 
     setlocale(LC_ALL,"");
-    
 
+    int valorA, valorB, valorC, temp;
+
+    printf("Digite três valores: ");
+        scanf("%d%d%d",&valorA,&valorB,&valorC);
+
+     if (valorA > valorB) {
+        temp = valorA;
+        valorA = valorB;
+        valorB = temp;
+    }
+    if (valorA > valorC) {
+        temp = valorA;
+        valorA = valorC;
+        valorC = temp;
+    }
+    if (valorB > valorC) {
+        temp = valorB;
+        valorB = valorC;
+        valorC = temp;
+    }
+
+    printf("\nValores em ordem crescente: %d %d %d\n", valorA, valorB, valorC);
 
     return 0;
 }
