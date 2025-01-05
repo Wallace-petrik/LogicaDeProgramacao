b698498d7203cfbbd52c2ce2ce0a8b4b5040b10f
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-01-05 13:01:51 -0300

    Capítulo 5.1: Exercício 1H

---
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo5/ExercicioDeFixacao5.1/ExercicioDeFixacao-1H/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_ALL,"");
+
+    int contador = 0;
+
+    int anterior = 1;
+    int atual = 1;
+    int proximo = 0;
+
+    while(contador++<=15){
+
+        printf("%d\n",anterior);
+        proximo = anterior+atual;
+        anterior = atual;
+        atual = proximo;
+    }
+
+    return 0;
+}
