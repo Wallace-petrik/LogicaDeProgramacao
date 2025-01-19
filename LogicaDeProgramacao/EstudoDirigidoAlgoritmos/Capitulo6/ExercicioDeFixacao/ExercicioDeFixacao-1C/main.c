edbc3454d87b91aeeed5c28bbc9a035706a632b8
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-01-19 07:53:30 -0300

    Capítulo 6 : Exercício 1C

--- LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo6/ExercicioDeFixacao/ExercicioDeFixacao-1C/main.c
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo6/ExercicioDeFixacao/ExercicioDeFixacao-1C/main.c
@@ -8,18 +8,24 @@ int main(){
 
     int vetA[tam];
     int vetB[tam];
-
+    int fib;
     for(int i = 0; i < tam; i++){
         printf("Digite %dª número: ",i+1);
             scanf("%d",&vetA[i]);
     }
 
-
+    for(int i = 0; i < tam; i++){
+        fib = 1;
+        for(int j  = 1; j <= vetA[i]; j++){
+           fib *= j;
+        }
+        vetB[i] = fib;
+    }
 
     printf("\n");
     for(int i = 0; i < tam; i++){
 
-        printf("Fatorial de %d = %d\n",vetA[i],vetB[i]);
+        printf("Fatorial de %d! = %d\n",vetA[i],vetB[i]);
 
     }
     return 0;
