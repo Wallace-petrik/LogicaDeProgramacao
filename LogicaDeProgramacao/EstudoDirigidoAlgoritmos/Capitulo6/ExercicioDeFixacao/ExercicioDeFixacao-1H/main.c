1dc1fcb5a513114ffa673160b925f486c5469839
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-01-24 05:38:16 -0300

    Capítulo 6: Exercício 1H

---
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo6/ExercicioDeFixacao/ExercicioDeFixacao-1H/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+#define tam 5
+int main(){
+
+    setlocale(LC_ALL,"");
+
+    int vetA[tam];
+    int vetB[tam];
+
+    for(int i  = 0; i < tam; i++){
+        printf("Entre com o %dª elemento do vetor: ",i);
+            scanf("%d",&vetA[i]);
+    }
+    for(int i = 0; i < tam; i++){
+        vetB[tam-i-1] = vetA[i];
+    }
+    for(int i = 0; i < tam; i++){
+        printf("VetA[%d] = %d\tVetB[%d] = %d\n",i,vetA[i],i,vetB[i]);
+    }
+
+
+    return 0;
+}
