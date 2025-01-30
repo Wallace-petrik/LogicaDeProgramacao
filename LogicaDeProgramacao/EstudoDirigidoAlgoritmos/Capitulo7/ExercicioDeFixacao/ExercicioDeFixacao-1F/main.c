10a4f5974765399594faac48b4dfb7d62adfa64d
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-01-30 07:30:10 -0300

    Capítulo 7: Exercício 1F
    Rotina de busca no vetor B

--- LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo7/ExercicioDeFixacao/ExercicioDeFixacao-1F/main.c
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo7/ExercicioDeFixacao/ExercicioDeFixacao-1F/main.c
@@ -9,6 +9,7 @@ int main(){
     setlocale(LC_ALL,"");
 
     int vetA[tam], vetB[tam];
+    int aux,continua,achou = tam;
 
     for(int i = 0; i < tam; i++){
         printf("Digite o %dº do vetor: ",i+1);
@@ -16,7 +17,23 @@ int main(){
         vetB[i] = pow(vetA[i],3);
     }
 
+    do{
+        printf("\nDigite o valor que deseja buscar no vetor B: ");
+            scanf("%d",&aux);
+        for(int i = 0; i < tam; i++){
+            if(aux==vetB[i]){
+                achou = i;
+                break;
+            }
+        }
+        if(achou >= 0 && achou <tam){
+            printf("\nValor encontrado Posição [%d], valor = %d.\n\n",achou,vetB[achou]);
+        }else printf("Valor não encontrado.");
 
+        printf("Deseja fazer outra busca:\n 1/Sim 0/Não: ");
+            scanf("%d",&continua);
+            achou = tam;
+    }while(continua != 0);
 
     return 0;
 }
