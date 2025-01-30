c98f738e93b289415e460e3bc6bd04a0726d63e3
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-01-30 07:12:50 -0300

    Capítulo 7: Exercício 1F
    Elemento do vetor B e o correnpondente de A ao Cubo

--- LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo7/ExercicioDeFixacao/ExercicioDeFixacao-1F/main.c
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo7/ExercicioDeFixacao/ExercicioDeFixacao-1F/main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
+
+#define tam 5
 
 int main(){
 
     setlocale(LC_ALL,"");
-    
+
+    int vetA[tam], vetB[tam];
+
+    for(int i = 0; i < tam; i++){
+        printf("Digite o %dº do vetor: ",i+1);
+            scanf("%d",&vetA[i]);
+        vetB[i] = pow(vetA[i],3);
+    }
+
 
 
     return 0;
