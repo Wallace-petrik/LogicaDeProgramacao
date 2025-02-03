eb83c43558258c47e765550c5215eef9c5bf1078
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-02-03 20:16:24 -0300

    Capítulo 8: Exercício 1D Vetror soma mais 5

---
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo8/ExercicioDeFixacao/ExercicioDeFixacao-1D/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_ALL,"");
+
+    int vetor[10];
+    int matriz[3][10];
+
+    for(int j = 0; j < 10; j++){
+         printf("Entre com o valor da vetor[%d]: ",j+1);
+            scanf("%d",&vetor[j]);
+    }
+
+    for(int j = 0; j < 10; j++){
+         matriz[0][j] = vetor[j] + 5;
+    }
+
+    return 0;
+}
