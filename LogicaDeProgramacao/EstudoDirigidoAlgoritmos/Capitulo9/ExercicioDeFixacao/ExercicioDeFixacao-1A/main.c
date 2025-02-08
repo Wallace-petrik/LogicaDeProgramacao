df0c9bd8f0026afd60c4af0d81d102fe0e563dcb
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-02-08 20:00:04 -0300

    Capítulo 9: Excercício 1C Implementação de ordenação de dados bubble sort

--- LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo9/ExercicioDeFixacao/ExercicioDeFixacao-1A/main.c
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo9/ExercicioDeFixacao/ExercicioDeFixacao-1A/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
-#define tam 2
+#define tam 3
 
 struct {
 
@@ -23,15 +23,13 @@ int main(){
         system("cls");
         printf("1 para Cadastrar contato~\n");
         printf("2 para Pesquisar contato\n");
-        printf("3 para Ordenar por nome\n");
-        printf("4 para listar contatos\n");
-        printf("5 para sair\n:");
+        printf("3 para listar contatos\n");
+        printf("4 para sair\n:");
             scanf("%d",&opcao);
 
         switch(opcao){
-            case 1:
-
-                system("cls");
+            case 1:{
+            system("cls");
                 for(int i = 0; i < tam; i++){
                     system("cls");
                     printf("Contato %d\n",i+1);
@@ -46,11 +44,11 @@ int main(){
                         scanf("%d",&minhaAgenda[i].telefone);
                         system("pause");
                 }
-
             break;
-            case 2:
+            }
 
-                system("cls");
+            case 2:{
+            system("cls");
                 achou = 0;
                 printf("Digite o nome que deseja buscar: \n");
                     fflush(stdin);
@@ -66,30 +64,54 @@ int main(){
                     }
                 }
                 if(achou == 1){
-                    printf("O nome %s foi localizado no index: %d\n",minhaAgenda[index].nome,index
-                           );
-                }else
+                    printf("O nome %s foi localizado no index: %d\n",minhaAgenda[index].nome,index);
+                }else{
                     printf("Nome não localizado\n");
+                }
                 system("pause");
 
             break;
-            case 3:
+            }
 
-            break;
-            case 4:
+            case 3:{
+                int continua = 0;
+                int fim = tam;
 
+                do{
+                    for(int i = 0; i < tam-1; i++){
+                        if(strcmp(minhaAgenda[i].nome,minhaAgenda[i+1].nome)>0){
+                            auxAgenda = minhaAgenda[i];
+                            minhaAgenda[i] = minhaAgenda[i+1];
+                            minhaAgenda[i+1] = auxAgenda;
+                        }
+                    }
+                    fim--;
+                }while(continua!=0);
+
+
+                printf("\n");
+                system("pause");
             break;
-            case 5:
+            }
+
+
+
+            case 4:{
                 printf("\n\tAté logo!!!");
-            break;
-            default:
+                break;
+            }
 
+
+            default:{
                 printf("\n\tOpição invalida!!!\n");
                 system("pause");
+            }
+
+
 
         }
 
-    }while(opcao != 5);
+    }while(opcao != 4);
 
     return 0;
 }
