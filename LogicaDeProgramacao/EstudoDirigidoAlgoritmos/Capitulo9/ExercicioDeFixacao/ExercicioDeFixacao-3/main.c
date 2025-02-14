65e8f11d578fe0b2f9aafd08aa709818d6864244
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-02-14 07:32:46 -0300

    Capítulo 9: Exercício 3C Implementação de busca por pessoas maiores que 1,5 de altura

--- LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo9/ExercicioDeFixacao/ExercicioDeFixacao-3/main.c
+++ LogicaDeProgramacao/EstudoDirigidoAlgoritmos/Capitulo9/ExercicioDeFixacao/ExercicioDeFixacao-3/main.c
@@ -67,7 +67,19 @@ int main(){
             break;}
 
             case 3:{
+                system("cls");
+                int controle = 0;
 
+                for(int i = 0; i < elem; i++){
+                    if(pessoa[i].altura>1.5){
+                        printf("%s\t%.2f",pessoa[i].nome,pessoa[i].altura);
+                        controle = 1;
+                    }
+                }
+                if(controle==0){
+                    printf("Nenhuma pessoa encontrada !!!");
+                }
+                getchar();
             break;}
 
             case 4:{
