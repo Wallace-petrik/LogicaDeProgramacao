24f29576b53c1f55b51e73ae3d150da020e9cf10
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-07-04 07:40:23 -0300

    Refatoração do Exercício de fixação 1.1
    Declaração da estrutura Livro

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosDeFixacao/ExerciciosDeFixacao1.1/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosDeFixacao/ExerciciosDeFixacao1.1/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <locale.h>
 
+typedef struct{
+    int codigoDoLivro;
+    char titulo[30];
+    char autor[30];
+    char assunto[30];
+    char editora[30];
+    int ano;
+    int edicao;
+}Livro;
+
+FILE *arqLivros;
+
 int main(){
 
-setlocale(LC_ALL,"");
+    setlocale(LC_ALL,"");
 
 
 
-return 0;
-}
 
+
+return 0;}
