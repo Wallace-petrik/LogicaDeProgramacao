015a588270000440fd236af1f03f162719cf339a
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-07-06 13:59:29 -0300

    Implementação do procedimento Menu

--- LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosDeFixacao/ExerciciosDeFixacao1.1/main.c
+++ LogicaDeProgramacao/LogicaDeProgramacaoConstrucaoDeAlgoritmosEstruturasDeDados/Capitulo05/ExerciciosDeFixacao/ExerciciosDeFixacao1.1/main.c
@@ -11,14 +11,38 @@ typedef struct{
     int edicao;
 }Livro;
 
-FILE *arqLivros;
-
 int main(){
 
     setlocale(LC_ALL,"");
 
-
-
-
+    menu();
 
 return 0;}
+
+void menu(){
+    int opcao;
+    do{
+        printf("1/p Cadastrar livro\n");
+        printf("2/p Buscar livros por assunto\n");
+        printf("3/p sair\n");
+            scanf("%d",&opcao);
+            fflush(stdin);
+        switch(opcao){
+            case 1:{
+
+            break;}
+            case 2:{
+
+            break;}
+            case 3:{
+                printf("Até logo !!!");
+                getchar();
+            break;}
+            default:{
+                printf("Opção invalida !!!");
+                getchar();
+                system("cls");}
+        }
+    }while(opcao!=3);
+}
+
